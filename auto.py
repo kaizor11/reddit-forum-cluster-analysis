770b49f389c1cc3def0552d02287f70638254fda
Your Correct Name <your_correct_email@example.com>
2026-03-06 15:18:13 -0800

    added word2vec + bow

--- auto.py
+++ auto.py
@@ -2,6 +2,7 @@ import sys
 import time
 from datasets import load_dataset
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.models.word2vec import Word2Vec
 from gensim.utils import tokenize
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
@@ -16,7 +17,7 @@ from MongoDBconnection import MongoDBConnection
 from scraper import scrape_subreddit, scrape_posts, SUBREDDIT_URL_OLD
 from wordcloud import WordCloud, STOPWORDS
 import math
-nltk.download('stopwords')
+# nltk.download('stopwords')
 
 
 scrape_interval_min = int(sys.argv[1])
@@ -80,7 +81,7 @@ def preprocess_json(json_file):
         dataset.append(temp)
     return dataset
 
-def cluster(in_docs):
+def cluster(in_docs, ax, model_name):
     tokens = [list(tokenize(x, to_lower=True)) for x in in_docs]
 
     #grab stopwords corpus
@@ -100,18 +101,36 @@ def cluster(in_docs):
 
     docs = [[token for token in doc if token not in stops] for doc in tokens]
 
-    inset = [TaggedDocument(doc, [i]) for i, doc in enumerate(docs)]
+    if model_name == "d2v":
+        print(f"Model: Doc2Vec")
+        inset = [TaggedDocument(doc, [i]) for i, doc in enumerate(docs)]
 
-    # generate embeddings of our texts
-    model = Doc2Vec(inset, 
-                    vector_size=5, 
-                    window=2, 
-                    min_count=1,
-                    workers=4)
+        # generate embeddings of our texts
+        model = Doc2Vec(inset, 
+                        vector_size=5, 
+                        window=2, 
+                        min_count=1,
+                        workers=4)
 
-    # turn texts into vectors by using the model
-    out = [model.infer_vector(x) for x in docs]
-    #print(out[:5])
+        # turn texts into vectors by using the model
+        out = [model.infer_vector(x) for x in docs]
+        #print(out[:5])
+    elif model_name == "w2v":
+        print(f"Model: Word2Vec + BoW")
+        model = Word2Vec(docs,
+                        vector_size=5,
+                        window=2,
+                        min_count=1,
+                        workers=4)
+
+        # Aggregate word vectors per document by averaging (BoW)
+        def bow(doc):
+            vectors = [model.wv[word] for word in doc if word in model.wv]
+            if len(vectors) == 0:
+                return np.zeros(5)  # fallback for empty docs
+            return np.mean(vectors, axis=0)
+
+        out = [bow(doc) for doc in docs]
 
     # define and fit cluster model
     cluster_model = KMeans(n_clusters=5,
@@ -158,23 +177,31 @@ def cluster(in_docs):
         cursor.annotation.set_text(f'{in_docs[doc_ind]}')
 
 
-    fig = plt.figure()
-    #ax = fig.add_subplot(projection='3d')
-    plt.scatter(points_x, 
-                points_y, 
-                #points_z, 
-                c=preds
-                )
-    plt.tight_layout()
+    # fig = plt.figure()
+    # #ax = fig.add_subplot(projection='3d')
+    # plt.scatter(points_x, 
+    #             points_y, 
+    #             #points_z, 
+    #             c=preds
+    #             )
+    # plt.tight_layout()
 
-    cursor = mpl.cursor(hover=True)
+    # cursor = mpl.cursor(hover=True)
+    # cursor.connect("add", display_annotations)
+    # # plt.savefig(f"scatter_{model_name}.png")
+    # plt.show()
+
+    # plot graphs together
+    ax.scatter(points_x, points_y, c=preds)
+    ax.set_title("Doc2Vec" if model_name == "d2v" else "Word2Vec + BoW")
+
+    cursor = mpl.cursor(ax, hover=True)        # <-- scope cursor to this ax
     cursor.connect("add", display_annotations)
-    plt.show()
 
     # predict clusters
     return preds, [" ".join(doc) for doc in docs]
 
-def plot_wordclouds(df, text_col, cluster_col):
+def plot_wordclouds(df, text_col, cluster_col, model_name):
     if df.empty: return
     
     clusters = sorted(df[cluster_col].unique())
@@ -198,41 +225,50 @@ def plot_wordclouds(df, text_col, cluster_col):
         plt.axis("off")
 
     plt.tight_layout()
-    plt.savefig("cluster_result.png") 
-    print("Wordcloud saved as 'cluster_result.png'")
-    plt.show()
-
+    plt.savefig(f"wordcloud_{model_name}.png") 
+    # print("Wordcloud saved as 'cluster_result.png'")
+    # plt.show()
 
 
 def main():
 
-    # while True:
-    for i in range(1):
+    while True:
+    # for i in range(1):
         #scrape data
-        scrape()
+        # scrape()
 
-         #store data
-        #store()
+        #store data
+        # store()
 
         # retrieve data
-        data_in = retrieve_data()
+        # data_in = retrieve_data()
 
         # read data
-        input = preprocess(data_in)
-        #input = preprocess_json("posts.json")
+        # input = preprocess(data_in) # old
+        input = preprocess_json("posts.json") # new
 
         # cluster data
-        cluster(input)
-        preds, out_docs = cluster(input)
-        
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+        fig.suptitle("Cluster Comparison: Doc2Vec vs Word2Vec + BoW")
+        d_preds, d_out_docs = cluster(input, model_name="d2v", ax=ax1)
+        w_preds, w_out_docs = cluster(input, model_name="w2v", ax=ax2)
+        plt.tight_layout()
+        plt.show()
 
         print("Preparing visualization...")
         df = pd.DataFrame({
-            'title': out_docs,   
-            'cluster_id': preds    
+            'title': d_out_docs,   
+            'cluster_id': d_preds    
         })
-        print("Generating wordclouds")
-        plot_wordclouds(df, text_col='title', cluster_col='cluster_id')
+        print("Generating wordclouds for D2V")
+        plot_wordclouds(df, text_col='title', cluster_col='cluster_id', model_name="d2v")
+
+        df = pd.DataFrame({
+            'title': w_out_docs,   
+            'cluster_id': w_preds    
+        })
+        print("Generating wordclouds for W2V")
+        plot_wordclouds(df, text_col='title', cluster_col='cluster_id', model_name="w2v")
         
         print(f"Sleeping for {scrape_interval_min} minutes")
         time.sleep(scrape_interval_min * 60)
